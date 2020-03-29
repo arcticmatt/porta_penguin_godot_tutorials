dc9a702cf93cffdd659a6ff0cc64a34779cf53eb
Matt Lim <mlimcollege@gmail.com>
2020-03-28 20:01:35 -0700

    [tutorial1] [edit] Add return type to _ready() function in Background.gd.

--- scripts/Background.gd
+++ scripts/Background.gd
@@ -3,7 +3,7 @@ extends Sprite
 const VELOCITY: float = -1.5
 var g_texture_width: float = 0
 
-func _ready():
+func _ready() -> void:
 	g_texture_width = texture.get_size().x * scale.x 
 	
 func _process(delta: float) -> void:
